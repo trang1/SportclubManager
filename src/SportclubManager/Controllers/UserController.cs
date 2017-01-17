9ec88a39ba44a07ccd40128094514359f3ede624
trang1 <trang@bk.ru>
2017-01-17 18:12:11 +0300

    Start managing users

---
+++ src/SportclubManager/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportclubManager.Models;
+
+namespace SportclubManager.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = Db.Users.OrderBy(u => u.UserID).ToList();
+            return View(users);
+        }
+
+        public ActionResult EditUser()
+        {
+            return View(CurrentUser);
+        }
+
+        public ActionResult ShowProfile(int userId)
+        {
+            return View(userId);
+        }
+    }
+}

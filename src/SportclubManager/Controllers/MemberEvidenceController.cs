ed728406f6edf145ba3ab0ec792b4252047fe5d5
trang1 <trang@bk.ru>
2017-01-30 18:13:32 +0300

    Member Evidence started

---
+++ src/SportclubManager/Controllers/MemberEvidenceController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportclubManager.Models;
+
+namespace SportclubManager.Controllers
+{
+    public class MemberEvidenceController : BaseController
+    {
+        // GET: MemberEvidence
+        public ActionResult Index()
+        {
+            var mem = new MemberEvidenceModel();
+
+            if (mem.Groups.Any())
+            {
+                mem.MemberEvidences = new List<MemberEvidenceWeek>();
+                mem.CurrentDate = DateTime.Today.ToShortDateString();
+
+                var group = mem.Groups.First();
+
+                int currentDayOfWeek = (int) DateTime.Today.DayOfWeek;
+                var sunday = DateTime.Today.AddDays(-currentDayOfWeek);
+                var monday = sunday.AddDays(1);
+                // If we started on Sunday, we should actually have gone *back*
+                // 6 days instead of forward 1...
+                if (currentDayOfWeek == 0)
+                {
+                    monday = monday.AddDays(-7);
+                }
+                var days = new[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+                foreach (var member in group.Members)
+                {
+                    var mew = new MemberEvidenceWeek {MemberName = member.FullName};
+
+                    for (var i = 0; i < 7; i++)
+                    {
+                        var date = monday.AddDays(i);
+                        var day = days[i];
+
+                        var me = Db.MemberEvidences.FirstOrDefault(m => m.Date == date && m.MemberID == member.MemberID);
+                        if (me == null)
+                        {
+                            me = new MemberEvidence {Date = date, GroupID = group.GroupID, MemberID = member.MemberID};
+                            Db.MemberEvidences.InsertOnSubmit(me);
+                            Db.MemberEvidences.Context.SubmitChanges();
+                        }
+                        mew[day] = me;
+                    }
+
+                    mem.MemberEvidences.Add(mew);
+                }
+            }
+
+            return View(mem);
+        }
+
+        public ActionResult Save()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/SportclubManager/Models/MemberEvidenceModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportclubManager.Auth;
+
+namespace SportclubManager.Models
+{
+    public class MemberEvidenceModel
+    {
+        public MultiSelectList GroupList
+        {
+            get
+            {
+                return new MultiSelectList(Groups, "GroupID", "GroupName");
+            }
+        }
+
+        public IList<Group> Groups
+        {
+            get
+            {
+                var db = DependencyResolver.Current.GetService<SportclubManagerDataContext>();
+                return db.Groups.Where(g => g.CoachID == CurrentUser.UserID).OrderBy(g => g.GroupName).ToList();
+            }
+        }
+
+        public User CurrentUser
+        {
+            get
+            {
+                var auth = DependencyResolver.Current.GetService<IAuthentication>();
+                return ((IUserIdentity)auth.CurrentUser.Identity).User;
+            }
+        }
+
+        public string CurrentDate { get; set; }
+
+        public IList<MemberEvidenceWeek> MemberEvidences { get; set; } 
+    }
+
+    public class MemberEvidenceWeek : Dictionary<string,MemberEvidence>
+    {
+        public string MemberName { get; set; }
+    }
+}
